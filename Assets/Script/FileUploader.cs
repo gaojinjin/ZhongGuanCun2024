dc353fcc2ad2ae9129e3353d881a57c5de4a3920
jinjin gao <gaojinjingg@163.com>
2024-04-11 11:04:47 +0800

    完成Unity 文件上传功能

---
+++ Assets/Script/FileUploader.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using UnityEngine;
+using UnityEngine.Networking;
+using System.IO;
+
+public class FileUploader : MonoBehaviour
+{
+    IEnumerator UploadFile(string filePath, string uploadURL)
+    {
+        byte[] fileBytes = File.ReadAllBytes(filePath);
+        WWWForm form = new WWWForm();
+        form.AddBinaryData("file", fileBytes, Path.GetFileName(filePath));
+
+        UnityWebRequest uwr = UnityWebRequest.Post(uploadURL, form);
+        yield return uwr.SendWebRequest();
+
+        if (uwr.result != UnityWebRequest.Result.Success)
+        {
+            Debug.LogError(uwr.error);
+        }
+        else
+        {
+            Debug.Log("文件上传成功！");
+        }
+    }
+
+    // 调用示例
+    void Start()
+    {
+        string filePath = "C:/Users/ASUS/Desktop/b.mp4";
+        string uploadURL = "https://kiwistudio.top/uploads/upload.php"; // 更改为你的服务器URL
+        StartCoroutine(UploadFile(filePath, uploadURL));
+    }
+}
