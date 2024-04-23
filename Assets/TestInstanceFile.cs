ed59f4017123fd4f517cce27314b8ad37d60181e
jinjin gao <gaojinjingg@163.com>
2024-04-23 17:51:29 +0800

    增加了云端修改星球效果，但是材质丢失问题，预测是因为win 10端不支持 ios端的addressable包，还没有测试

--- Assets/TestInstanceFile.cs
+++ Assets/TestInstanceFile.cs
@@ -2,6 +2,8 @@ using System.Collections;
 using System.Collections.Generic;
 using System.ComponentModel;
 using UnityEngine;
+using UnityEngine.AddressableAssets;
+using UnityEngine.ResourceManagement.AsyncOperations;
 using UnityEngine.UI;
 using UnityEngine.XR.ARFoundation;
 using UnityEngine.XR.ARSubsystems;
@@ -17,5 +19,16 @@ public class TestInstanceFile : MonoBehaviour
             Instantiate(InstanceGo);
 
         });
+
+        Addressables.LoadAssetAsync<GameObject>("Show").Completed += OnAssetLoaded;
+    }
+
+    private void OnAssetLoaded(AsyncOperationHandle<GameObject> obj)
+    {
+        if (obj.Status == AsyncOperationStatus.Succeeded)
+        {
+            GameObject loadedAsset = obj.Result;
+            Instantiate(loadedAsset);
+        }
     }
 }
