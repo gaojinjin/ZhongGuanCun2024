e052c46361c7d0033d947eff2f66760e60949ca4
jinjin gao <gaojinjingg@163.com>
2024-04-26 14:36:42 +0800

    更新链接方式为跳过域名直接链接服务器

--- Assets/QRGenerator/EasyQRCode.cs
+++ Assets/QRGenerator/EasyQRCode.cs
@@ -17,7 +17,7 @@ public class EasyQRCode : MonoBehaviour {
         // Debug.Log(" url  info  "+ url);
         // Example usage of QR Generator
         // The text can be any string, link or other QR Code supported string
-        textToEncode = "https://kiwistudio.top/uploads/GetInfo.php?file=uploads/" + url;
+        textToEncode = "https://101.126.83.110/uploads/GetInfo.php?file=uploads/" + url;
         Texture2D qrTexture = QRGenerator.EncodeString(textToEncode, darkColor, lightColor);
 
         // Set the generated texture as the mainTexture on the quad
